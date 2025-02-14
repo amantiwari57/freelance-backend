b8d4dd97897942b4031c54724dd59b658bdbdea9
mrcoder57 <tiwariji2300@gmail.com>
2025-02-14 13:16:56 +0530

    feature:add redis client

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import signup from './auth/signup'
 import { connectDB } from '../helper/dbConnect'
 import login from './auth/login'
 import googleAuth from './auth/googleAuth'
+import test from './test/test'
 const app = new Hono()
 
 connectDB()
 app.route('/',signup)
 app.route('/',login)
 app.route('/',googleAuth)
+app.route('/',test)
 app.get('/', (c) => {
  
   return c.text('Hello Hono!')
