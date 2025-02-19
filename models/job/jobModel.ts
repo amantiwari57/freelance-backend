609843614900916015c6dacbb7f0158581e5aa90
mrcoder57 <tiwariji2300@gmail.com>
2025-02-19 19:58:09 +0530

    feayure: create job model

---
+++ models/job/jobModel.ts
@@ -0,0 +1,69 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IJob extends Document {
+  userId: mongoose.Types.ObjectId;
+  userType: "client";
+  jobTitle: string;
+  description: string;
+  skills: string[];
+  timeline: "small" | "medium" | "large";
+  totalTime: "1 month" | "3 months" | "6monthsormore";
+  expertiseLevel: "entry" | "intermediate" | "expert";
+  paymentType: "fixed" | "hourly";
+  price?: number; // Only for fixed payments
+  pricePerHour?: { min: number; max: number }; // Only for hourly payments
+  files: string[];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const JobSchema: Schema = new Schema<IJob>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    userType: { type: String, enum: ["client"], required: true },
+    jobTitle: { type: String, required: true },
+    description: { type: String, required: true },
+    skills: { type: [String], required: true },
+    timeline: {
+      type: String,
+      enum: ["small", "medium", "large"],
+      required: true,
+    },
+    totalTime: {
+      type: String,
+      enum: ["1 month", "3 months", "6monthsormore"],
+      required: true,
+    },
+    expertiseLevel: {
+      type: String,
+      enum: ["entry", "intermediate", "expert"],
+      required: true,
+    },
+    paymentType: { type: String, enum: ["fixed", "hourly"], required: true },
+    price: {
+      type: Number,
+      required: function () {
+        return this.paymentType === "fixed";
+      },
+    },
+    pricePerHour: {
+      min: {
+        type: Number,
+        required: function () {
+          return this.paymentType === "hourly";
+        },
+      },
+      max: {
+        type: Number,
+        required: function () {
+          return this.paymentType === "hourly";
+        },
+      },
+    },
+    files: { type: [String], default: [] },
+  },
+  { timestamps: true }
+);
+
+const Job = mongoose.model<IJob>("Job", JobSchema);
+export default Job;
