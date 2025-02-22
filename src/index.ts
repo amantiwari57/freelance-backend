0ca092a36885c8798e2f814e8d6e79bf6d9318cf
mrcoder57 <tiwariji2300@gmail.com>
2025-02-22 20:46:29 +0530

    feature: add new routes

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,36 @@
-import { Hono } from 'hono'
-import signup from './auth/signup'
-import { connectDB } from '../helper/dbConnect'
-import login from './auth/login'
-import googleAuth from './auth/googleAuth'
-import test from './test/test'
-import forgotPassword from './auth/forgot-password'
-import profile from './profile/profile'
-import upload from './upload/upload'
-import jobRouter from './jobs/jobs'
-import clientProfile from './client/clientProfile'
-const app = new Hono()
+import { Hono } from "hono";
+import signup from "./auth/signup";
+import { connectDB } from "../helper/dbConnect";
+import login from "./auth/login";
+import googleAuth from "./auth/googleAuth";
+import test from "./test/test";
+import forgotPassword from "./auth/forgot-password";
+import profile from "./profile/profile";
+import upload from "./upload/upload";
+import jobRouter from "./jobs/jobs";
+import clientProfile from "./client/clientProfile";
+import refreshTrackerRouter from "./refreshProposals/refreshProposals";
+import jobMilestoneRouter from "./jobs/jobMilestone/jobMilestone";
+import proposalMilestoneRouter from "./proposals/proposalMilestone/proposalMilestone";
+import proposalRouter from "./proposals/proposals";
+const app = new Hono();
 
-connectDB()
-app.route('/',signup)
-app.route('/',login)
-app.route('/',googleAuth)
-app.route('/',test)
-app.route('/',forgotPassword)
-app.route('/',profile)
-app.route('/',upload)
-app.route('/',jobRouter)
-app.route('/', clientProfile)
-app.get('/', (c) => {
- 
-  return c.text('Hello Hono!')
-})
+connectDB();
+app.route("/", signup);
+app.route("/", login);
+app.route("/", googleAuth);
+app.route("/", test);
+app.route("/", forgotPassword);
+app.route("/", profile);
+app.route("/", upload);
+app.route("/", jobRouter);
+app.route("/", clientProfile);
+app.route("/", refreshTrackerRouter);
+app.route("/", jobMilestoneRouter);
+app.route("/", proposalMilestoneRouter);
+app.route("/", proposalRouter);
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+export default app;
